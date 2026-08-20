a1f866df4b16f554dc27d67517544b85f7c808ee
parth <prajapatiparth70305@gmail.com>
2026-08-20 10:31:27 +0530

    file

--- promt_engineering/settings.py
+++ promt_engineering/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-h*%8qtiq*slee_a8&)@+_#bk_yeqta2^3l&7zt1jhrgkj(#uxn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
